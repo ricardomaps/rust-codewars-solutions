5c5d14aada25727e543e1da121ea0afdd101dede
ricmaps <ricardomapurungajunior@gmail.com>
2026-02-04 16:36:16 -0300

    solved who likes it?

---
+++ rust-codewars-solutions/src/who_likes_it.rs
@@ -0,0 +1,14 @@
+fn likes(names: &[&str]) -> String {
+    match names.len() {
+        0 => "no one likes this".to_string(),
+        1 => format!("{} likes this", names[0]),
+        2 => format!("{} and {} like this", names[0], names[1]),
+        3 => format!("{}, {} and {} like this", names[0], names[1], names[2]),
+        _ => format!(
+            "{}, {} and {} others like this",
+            names[0],
+            names[1],
+            names.len() - 2
+        ),
+    }
+}
